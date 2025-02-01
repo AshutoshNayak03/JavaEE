8b943ed46526bfe79b8f0304c13b19773f89cfd0
Ashutosh Nayak <ashutoshnayak.official@gmail.com>
2025-02-01 10:27:59 +0530

    commit

--- wipro_javaee_project/src/com/wipro/javaee/HelloWorld.java
+++ wipro_javaee_project/src/com/wipro/javaee/HelloWorld.java
@@ -19,8 +19,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Thhis is my JavaEE project");
+		// TO DO Auto-generated method stub
 
 	}
 
